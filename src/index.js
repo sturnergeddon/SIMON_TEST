84ec530659ddb155ff76e14fb77d69e7ed3a319f
sturnergeddon <51706289+sturnergeddon@users.noreply.github.com>
2019-09-17 13:31:12 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+//import { createStore } from "redux";
 import { validator } from "email-validator";
 
 import "./styles.css";
+//import dataReducer from "./reducers/data";
+//import { Provider } from "react-redux";
+import SimpleStorage, {
+  clearStorage,
+  resetParentState
+} from "react-simple-storage";
 
 class NewForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formPost: "",
       email: ""
     };
     this.submitForm = this.submitForm.bind(this);
     this.validate = this.validate.bind(this);
     this.onChange = this.onChange.bind(this);
     this.reset = this.reset.bind(this);
-  }
-  onChange(e, field, value) {
-    //store the change
-    store.dispatch({ type: "set_field", field, value });
+    // store the component's initial state to reset it
+    this.initialState = this.state;
   }
 
+  componentDidMount() {
+    // let _this = this;
+    /* store.subscribe(function() {
+      _this.forceUpdate();
+    });*/
+  }
+
+  onChange(e, field, value) {}
+
   reset() {
-    store.dispatch({ type: "reset_data" });
+    //store.dispatch({ type: "RESET_DATA" });
   }
 
   submitForm(event) {
@@ -36,34 +48,42 @@ class NewForm extends React.Component {
     let email = this.state.email;
 
     console.log("Run Validation", valid);
-    console.log("Email", email);
+    console.log("Email in validation", email);
 
     if (email && !validator.validate(email)); // true
     valid = false;
 
     this.setState({ valid: valid });
-    console.log("valid state? ", valid);
+    //this.setState({ email: "test" });
+    // console.log("valid state? ", valid);
     return { valid };
   }
 
   render() {
     let valid = this.state.valid;
-    console.log("valid in render", valid);
+
+    console.log("First", this.state.first_name);
+    console.log("Last", this.state.last_name);
+    console.log("dob", this.state.date_of_birth);
+    console.log("Email", this.state.email);
+
     return (
       <div className="App">
         <div className="container">
-          <form onSubmit={this.submitForm}>
-            <h1>User Details</h1>
-            <h2>Please enter your details to continue:</h2>
+          <h1>User Details</h1>
+          <h2>Please enter your details to continue:</h2>
+          <SimpleStorage parent={this} prefix={"ParentComponent"} />
+          <form>
             <div>
               <label>
                 First Name:
                 <input
+                  id="first_name"
                   type="text"
                   name="first_name"
                   placeholder="e.g. John"
                   onBlur={this.validate}
-                  required={true}
+                  onChange={e => this.setState({ first_name: e.target.value })}
                 />
               </label>
             </div>
@@ -71,11 +91,12 @@ class NewForm extends React.Component {
               <label>
                 Last Name:
                 <input
+                  id="last_name"
                   type="text"
                   name="last_name"
                   placeholder="e.g. Doe"
                   onBlur={this.validate}
-                  required={true}
+                  onChange={e => this.setState({ last_name: e.target.value })}
                 />
               </label>
             </div>
@@ -83,10 +104,13 @@ class NewForm extends React.Component {
               <label>
                 Date of Birth:
                 <input
+                  id="date_of_birth"
                   type="date"
                   name="date_of_birth"
                   onBlur={this.validate}
-                  required={true}
+                  onChange={e =>
+                    this.setState({ date_of_birth: e.target.value })
+                  }
                 />
               </label>
             </div>
@@ -94,14 +118,27 @@ class NewForm extends React.Component {
               <label>
                 Email Address:
                 <input
+                  id="email"
                   type="email"
                   name="email"
                   onBlur={this.validate}
-                  required={true}
+                  onChange={e => this.setState({ email: e.target.value })}
                 />
               </label>
             </div>
             <input type="submit" value="Submit" disabled={!valid} />
+
+            <button
+              onClick={() =>
+                resetParentState(this, this.initialState, ["text"])
+              }
+            >
+              resetParentState
+            </button>
+
+            <button onClick={() => clearStorage("ParentComponent")}>
+              clearStorage
+            </button>
           </form>
         </div>
       </div>
@@ -110,4 +147,9 @@ class NewForm extends React.Component {
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<NewForm />, rootElement);
+ReactDOM.render(
+  // <Provider store={store}>
+  <NewForm />,
+  // </Provider>,
+  rootElement
+);
---
+++ src/reducers/data.js
@@ -0,0 +1,22 @@
+//REDUCER
+//import { _ } from "lodash";
+/*
+const dataReducer = (state = {}, action) => {
+  console.log("action", action);
+  switch (action.type) {
+    case "SET_FIELD":
+      //   return (state = { email: "TEST" });
+      state = (state, { ["form"]: { [action.field]: action.value } });
+
+      break;
+    case "RESET_DATA":
+      state = {};
+      break;
+    default:
+      // state = {};
+      console.log("Defaulted");
+      break;
+  }
+};
+
+export default dataReducer;*/
